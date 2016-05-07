05c6d4fb03e42b57ac38aff841adc9f21e124f57
shadowfool <aaronf101@gmail.com>
2016-05-06 17:31:52 -0700

    Create dynamic video list component, create dynamic video player component, progress on connecting video player and video list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,31 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      library: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+  }
 
+  clickVideoHandler(a) {
+    console.log(this);
+  }
+
+
+  render () {
+    return (
+     <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer currentVideo={this.state.currentVideo}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList onClick={this.clickVideoHandler} videos={this.state.library}/>
+        </div>
+      </div>
+		);
+  }
+}
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
